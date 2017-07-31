f1924db39d66d356a8e234b6778034a604c9578f
Rishi Giri <rushforlinux@gmail.com>
2017-08-01 00:36:07 +0530

    3 passed

---
+++ test.js
@@ -0,0 +1,35 @@
+import childProcess from 'child_process';
+import test from 'ava';
+
+test.cb('regular', t => {
+	const cp = childProcess.spawn('./cli.js', {stdio: 'inherit'});
+
+	cp.on('error', t.ifError);
+
+	cp.on('close', code => {
+		t.is(code, 1);
+		t.end();
+	});
+});
+
+test.cb('default', t => {
+	const cp = childProcess.spawn('./cli.js', ['-t'], {stdio: 'inherit'});
+
+	cp.on('error', t.ifError);
+
+	cp.on('close', code => {
+		t.is(code, 0);
+		t.end();
+	});
+});
+
+test.cb('specific', t => {
+	const cp = childProcess.spawn('./cli.js', ['-d', '2016-10-10'], {stdio: 'inherit'});
+
+	cp.on('error', t.ifError);
+
+	cp.on('close', code => {
+		t.is(code, 0);
+		t.end();
+	});
+});
